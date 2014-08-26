c05ddb4f9e4c57ddf6ff016a025cf21f2e68003d
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-08-26 09:19:52 +0900

    Test_Commonをabstractに

--- tests/Common.php
+++ tests/Common.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/Autoloader.php';
 
 define('MOCK_SERVER_PORT', 9876);
 
-class Test_Common extends PHPUnit_Framework_TestCase
+abstract class Test_Common extends PHPUnit_Framework_TestCase
 {
 
 }
