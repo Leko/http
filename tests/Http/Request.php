576c14a68f4feeb0c1489c2469af6f1cc4170264
Shingo Inoue <inoue.shingo@hamee.co.jp>
2014-08-26 09:20:18 +0900

    RequestとResponseもテスト対象に追加

---
+++ tests/Http/Request.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/../Common.php';
+
+class Test_Http_Request extends Test_Common
+{
+
+}
---
+++ tests/Http/Response.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once __DIR__.'/../Common.php';
+
+class Test_Http_Response extends Test_Common
+{
+
+}
